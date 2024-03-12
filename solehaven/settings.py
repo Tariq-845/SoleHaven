0cd44c165a3b19941945e16d532e1b0c84e9f998
Tariq Ebden <tebden16@gmail.com>
2024-03-12 11:51:54 +0000

    added: secret key

--- solehaven/settings.py
+++ solehaven/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ob9@t)u=i00tl-&qd=6x56^82o-w-^^6%c(#@(7tu1hf)ryu%3'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
