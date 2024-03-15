3e8efa4cc3b399c178d55ba0965289277b3bc05c
Tariq Ebden <tebden16@gmail.com>
2024-03-15 11:56:57 +0000

    add: review forms

--- products/views.py
+++ products/views.py
@@ -16,11 +16,15 @@ def product_page(request):
 
 def product_detail(request, pk):
   products = get_object_or_404(Product, pk=pk)
+  reviews = products.reviews.all()
+  review_count = products.reviews.all().count()
 
   return render(
     request,
     'products/products_detail.html',
     {
-      'products': products
+      'products': products,
+      'reviews': reviews,
+      'review_count': review_count,
     }
   )
