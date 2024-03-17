71050b7aa5bf7eb04b126f7612274054de68d2ff
Tariq Ebden <tebden16@gmail.com>
2024-03-17 19:58:40 +0000

    fix: debug settings

--- solehaven/settings.py
+++ solehaven/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
   '8000-tariq845-solehaven-go5dj7z0ahr.ws-eu110.gitpod.io',
@@ -100,12 +100,6 @@ WSGI_APPLICATION = 'solehaven.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
